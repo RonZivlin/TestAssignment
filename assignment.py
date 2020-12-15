b695a98f633a518740839d3e90a65992964e9a9e
Ron Zivlin <ron@zivlin.net>
2020-12-15 23:34:42 +0200

    added a script that reads http req and res from a file and prints the end-point and the method of each req

---
+++ assignment.py
@@ -0,0 +1,57 @@
+import enum
+
+#getting from user a file path to a file containing a sequence of HTTP 1.1 messages and their responses.
+filePath = input("enter file path:")
+f = open(filePath, "r", encoding="utf-8")
+lines = f.readlines()
+
+numOfMessage = 1
+endOfMessage = False
+
+crlf = ""
+
+for i in range(80):
+    crlf += "-"
+
+class message(enum.Enum):
+   firstLine = 1
+   header = 2
+   messageBody = 3
+
+messageSection = message.firstLine
+
+
+for line in lines:
+
+    if line == crlf or line == crlf + "\n":
+        if endOfMessage:
+
+            if numOfMessage % 2 == 0:
+                print("Res_" + str(int(numOfMessage/2)) + ":\n")
+            else:
+                print("Req_" + str(int(numOfMessage/2 + 1)) + ":\n")
+                print ("method: " + method)
+                print ("end-point: " + endPoint)
+                print()
+
+            numOfMessage += 1
+            messageSection = message.firstLine
+            endOfMessage = False
+
+        else:
+            endOfMessage = True
+
+        continue
+    else:
+        endOfMessage = False
+
+    if messageSection == message.firstLine:
+        words = line.split()
+        method = words[0]
+        endPoint = words[1]
+        messageSection = message.header
+        continue
+
+    if messageSection == message.header and line == "\n":
+        messageSection = message.messageBody
+        continue
